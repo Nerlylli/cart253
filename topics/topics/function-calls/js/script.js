63a1b9fcfce6123c43230fd227d785329b9b507d
Nerlylli <katenerlycadet@gmail.com>
2025-09-14 19:10:59 -0400

    all the changes
    I'm a bit confused.

---
+++ topics/topics/function-calls/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Function Calls
+ * Nerly Cadet
+ * 
+ * Working on the function calls examples.
+ */
+
+"use strict";
+
+
+function setup() {
+    createCanvas(640, 480);
+}
+
+function draw() {
+    background(250, 100, 100);
+    rect(200, 80, 240, 320);
+}
