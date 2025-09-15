d09907fe49cfc6e9f3b384fd077f02108aeaba3b
Nerlylli <katenerlycadet@gmail.com>
2025-09-14 20:09:00 -0400

    Instructions, drawing tutorial

---
+++ topics/drawing/js/script.js
@@ -0,0 +1,45 @@
+/**
+ * The Gayest Record of All Time
+ * Nerly Cadet
+ * 
+ * Displays the Gayest Record of All Time
+ */
+
+"use strict";
+
+/**
+ * creates a square canvas
+*/
+function setup() {
+    createCanvas(640, 640);
+}
+
+/**
+ * Displays the record
+*/
+
+function draw() {
+    // Grey background
+    background(150);
+
+    //The main part of the record
+    push();
+    fill(255, 0, 0);
+    stroke(255);
+    ellipse(320, 320, 480);
+    pop();
+
+    // The label on the record
+    push();
+    fill('white');
+    noStroke();
+    ellipse(320, 320, 140);
+    pop();
+
+    // The hole in the record
+    push();
+    fill('#000000');
+    noStroke();
+    ellipse(320, 320, 20);
+    pop();
+}
