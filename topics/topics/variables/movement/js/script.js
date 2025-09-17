3b29b6209d0ff49bdac3baba0495a18079c59b78
Nerlylli <katenerlycadet@gmail.com>
2025-09-17 19:11:55 -0400

    Started the movement

---
+++ topics/topics/variables/movement/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Variables in practice movement
+ * Nerly Cadet
+ * 
+ * Experimenting with movement
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
