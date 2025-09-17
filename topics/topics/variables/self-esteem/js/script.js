bf78ebfb2fcdf8688e3fdc7d1d083e1a2d451446
Nerlylli <katenerlycadet@gmail.com>
2025-09-17 18:02:43 -0400

    started the self-esteem project (JavaScript Objects)

---
+++ topics/topics/variables/self-esteem/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Self-Esteem
+ * Nerly Cadet
+ * 
+ * A portrait of Pippin's self-esteem on a sunny day.
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
