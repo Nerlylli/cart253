d5a8c0f729b8435f65b45d8e91b31a786c3a78d1
Nerlylli <katenerlycadet@gmail.com>
2025-09-17 19:54:48 -0400

    Finished debugging variables!

--- topics/topics/debugging/debugging-variables/js/script.js
+++ topics/topics/debugging/debugging-variables/js/script.js
@@ -8,30 +8,31 @@
 
 "use strict";
 
-const bgColor = "#87ceeb"; // Sky blue
+const bgColour = "#87ceeb"; // Sky blue
 
 // The bug
 const bug = {
     // Position and dimensions
-    x: 250
+    x: 250,
     y: 0,
     w: 20,
+    h: 30,
+
     // Movement
     velocity: {
-        x: -100,
-        y: -12, // Starts moving down
+        x: 0,
+        y: 12,  // Starts moving down
     },
     // Colour
-    color: "#87ceeb" // Dark brown
-
+    color: "#4e2006ff" // Dark brown
+}
 
 /**
  * Create the canvas
 */
 function setup() {
-        createCanvas(500, 500);
+    createCanvas(500, 500);
 }
-
 
 /**
  * Updates and draws the bug
@@ -44,8 +45,8 @@ function draw() {
 }
 
 function moveBug() {
-    bug.xx = bug.x + bug.velocity.x;
-    bug.y == bug.y + bug.velocity.y;
+    bug.x = bug.x + bug.velocity.x;
+    bug.y = bug.y + bug.velocity.y;
 }
 
 /**
@@ -56,14 +57,16 @@ function drawBug() {
     push();
     noStroke();
     fill(bug.color);
-    ellipse(bug.x, bug.y, bug.width, bug.height);
+    ellipse(bug.x, bug.y, bug.w, bug.h);
     pop();
 
     // Legs
     push();
-    stroke(bug.stroke);
+    stroke(bug.color);
+
     // Thicken the legs a bit
     strokeWeight(2);
+
     // Three lines horizontally across the body at different heights for the legs
     line(bug.x - bug.w, bug.y - bug.h / 4, bug.x + bug.w, bug.y - bug.h / 4);
     line(bug.x - bug.w, bug.y, bug.x + bug.w, bug.y);
