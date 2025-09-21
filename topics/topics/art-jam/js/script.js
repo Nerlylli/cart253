880f015bf2fc943f670300811a11c4ba42c886eb
Nerlylli <katenerlycadet@gmail.com>
2025-09-21 16:13:14 -0400

    Made the head
    Happy with the end result. Might not have to start again.

--- topics/topics/art-jam/js/script.js
+++ topics/topics/art-jam/js/script.js
@@ -11,7 +11,7 @@
  * Create Canvas
 */
 function setup() {
-    createCanvas(640, 640);
+    createCanvas(700, 700);
 }
 
 
@@ -23,4 +23,24 @@ function draw() {
     background(255, 207, 248);
 
     // Making a head
-}
+    //1. Circle
+    push();
+    fill('white');
+    ellipse(370, 250, 400);
+    pop();
+
+    //2. Jaw
+    push();
+    //Rotate the jaw
+    rotate(75);
+    rect(-15, 387, 200, 150);
+    pop();
+
+    //3. Neck
+    push();
+    //Rotate the neck
+    rotate(50);
+    rect(175, 495, 100, 200);
+    pop();
+
+};
