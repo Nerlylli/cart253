cb7cf4724c820ba2265cdfa360f9c98caffa0a6d
Nerlylli <katenerlycadet@gmail.com>
2025-10-23 14:54:31 -0400

    Finished step 1 through 4

--- topics/topics/functions-challenge/js/script.js
+++ topics/topics/functions-challenge/js/script.js
@@ -1,24 +1,133 @@
 /**
- * Title of Project
- * Author Name
+ * Bouncy Ball Ball Bonanza
+ * Nerly Cadet, Ya Xuan Pang, Ashmitha Kanagiah
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * The starting point for a ball-bouncing experience of
+ * epic proportions!
  */
 
 "use strict";
 
+//balls 
+let ball1 = undefined
+let ball2 = undefined
+
+// Our ball
+const ball = {
+    x: 300,
+    y: 20,
+    width: 10,
+    height: 10,
+    velocity: {
+        x: 0,
+        y: 5
+    }
+};
+
+// Our paddle
+const paddle = {
+    x: 300,
+    y: 280,
+    width: 80,
+    height: 10
+};
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create the canvas
 */
 function setup() {
-
+    createCanvas(600, 300);
 }
+
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Move and display the ball and paddle
 */
 function draw() {
+    background("#87ceeb");
+
+    movePaddle(paddle);
+    moveBall(ball);
+
+    handleBounce(ball, paddle);
+
+    drawPaddle(paddle);
+    drawBall(ball);
+}
+
+/**
+ * Moves the paddle
+ */
+function movePaddle(paddle) {
+    paddle.x = mouseX;
+
 
 }
+
+/**
+ * Moves the ball passed in as a parameter
+ */
+function moveBall(ball) {
+    ball.x += ball.velocity.x;
+    ball.y += ball.velocity.y;
+
+}
+
+/**
+ * Bounces the provided ball off the provided paddle
+ */
+function handleBounce(ball, paddle) {
+    //check if ball overlaps the paddle
+    const overlap = checkOverlap(ball, paddle);
+    //if there is an overlap, bounce the ball back up
+    if (overlap) {
+        ball.velocity.y *= -1;
+    }
+}
+
+function checkBallOverlap(ball, paddle) {
+    let result = (ball.x + ball.size > paddle.x &&
+        ball.x < paddle.x + paddle.width &&
+        ball.y + ball.size > paddle.y &&
+        ball.y < paddle.y + paddle.height);
+    return result;
+}
+
+/**
+ * Returns true if rectA and rectB overlap, and false otherwise
+ * Assumes rectA and rectB have properties x, y, width and height to describe
+ * their rectangles, and that rectA and rectB are displayed CENTERED on their
+ * x,y coordinates.
+ */
+function checkOverlap(rectA, rectB) {
+    return (rectA.x + rectA.width / 2 > rectB.x - rectB.width / 2 &&
+        rectA.x - rectA.width / 2 < rectB.x + rectB.width / 2 &&
+        rectA.y + rectA.height / 2 > rectB.y - rectB.height / 2 &&
+        rectA.y - rectA.height / 2 < rectB.y + rectB.height / 2);
+}
+
+/**
+ * Draws the specified paddle on the canvas
+ */
+function drawPaddle(paddle) {
+    push();
+    rectMode(CENTER);
+    noStroke();
+    fill("pink");
+    rect(paddle.x, paddle.y, paddle.width, paddle.height);
+    pop();
+}
+
+/**
+ * Draws the specified ball on the canvas
+ */
+function drawBall(ball) {
+    push();
+    rectMode(CENTER);
+    noStroke();
+    fill("pink");
+    rect(ball.x, ball.y, ball.width, ball.height);
+    pop();
+}
+
