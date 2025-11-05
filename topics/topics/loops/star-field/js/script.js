d4c37a9ab099d81e764d358b364f70fd933c02fb
Nerlylli <katenerlycadet@gmail.com>
2025-11-05 18:14:31 -0500

    Started the Star-Field project

---
+++ topics/topics/loops/star-field/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Star Field
+ * Nerly Cadet
+ * 
+ * Draws a star-field with a for-loop!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
